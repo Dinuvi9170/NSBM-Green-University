9e7c6b220df8ce88c873a3691be21900bc7541ce
Dinuvi9170 <135813670+Dinuvi9170@users.noreply.github.com>
2023-06-20 09:02:33 +0530

    Add files via upload

---
+++ 01Y02S/C#/Lab/tutorial question 3/tutorial question 3/CalculateValues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace tutorial_question_3
+{
+    internal class CalculateValues
+    {
+        public double addition(double num1, double num2)
+        {
+            
+            double add = num1 + num2;
+            return add;
+        }
+        public double substraction(double num1, double num2)
+        {
+            double sub = num1 - num2;
+            return sub;
+        }
+        public double multipication(double num1, double num2)
+        {
+            double mul = num1 * num2;
+            return mul;
+        }
+        public double division(double num1, double num2)
+        {
+            double div = num1 / num2;
+            return div ;
+        }
+    }
+}
---
+++ 01Y02S/C#/Lab/tutorial question 3/tutorial question 3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial_question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for addition");
+            Console.WriteLine("Enter 2 for substraction");
+            Console.WriteLine("Enter 3 for multipication");
+            Console.WriteLine("Enter 4 for division");
+
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number 1:");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter number 2:");
+            double num2 = double.Parse(Console.ReadLine());
+            CalculateValues obj = new CalculateValues();
+            
+            double add = obj.addition(num1, num2);
+            double sub = obj.substraction(num1, num2);
+            double mul = obj.multipication(num1, num2);
+            double div = obj.division(num1, num2);
+            if (choice == 1)
+                Console.Write("the addition :" + add);
+            else if (choice == 2)
+                Console.Write("the substraction :" + sub);
+
+            else if (choice == 3)
+                Console.Write("the multipication :" + mul);
+            else if (choice == 4)
+                if (num2 != 0)
+                    Console.Write("the division :" + div);
+                else
+                    Console.Write("Not defined");
+            else
+                Console.Write("enter a valid choice");
+
+           Console.ReadLine();
+        }
+    }
+}
