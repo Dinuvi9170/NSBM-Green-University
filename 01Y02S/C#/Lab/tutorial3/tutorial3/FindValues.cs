a0809e90399cbfb0b9e14aa36b6d46bab5a25433
Dinuvi9170 <135813670+Dinuvi9170@users.noreply.github.com>
2023-06-20 08:52:01 +0530

    upload

---
+++ 01Y02S/C#/Lab/tutorial3/tutorial3/FindValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial3
+{
+    internal class FindValues
+    {
+        public double findArea(double radius)
+        {
+             const double  PI = 3.14;
+            double area = PI * radius * radius;
+            return area;
+        }
+        public double findcircumference(double radius)
+        {
+            const double PI = 3.14;
+            double cir = 2 * PI * radius;
+            return cir;
+        }
+    }
+}
---
+++ 01Y02S/C#/Lab/tutorial3/tutorial3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the radius of the circle:");
+            double radius = double.Parse(Console.ReadLine());
+            
+            FindValues objfind = new FindValues();
+            double area= objfind.findArea(radius);
+            
+            Console.WriteLine("The area of the circle :"+area);
+            double cir = objfind.findcircumference(radius);
+            Console.WriteLine("The area of the circle :" +cir);
+
+            Console.ReadLine();
+        }
+    }
+}
