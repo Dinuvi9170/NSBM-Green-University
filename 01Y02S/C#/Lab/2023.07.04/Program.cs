2a4eb11c0321467492e65fc1e5007ddb888da4ac
Dinuvi9170 <135813670+Dinuvi9170@users.noreply.github.com>
2023-07-04 10:50:19 +0530

    Add files via upload

---
+++ 01Y02S/C#/Lab/2023.07.04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            number obj = new number();
+            int[] a= obj.array(size);
+
+            
+            Console.WriteLine("The array values are:");
+            foreach(int value in a)
+            {
+
+                Console.Write(value + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01Y02S/C#/Lab/2023.07.04/number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capital
+{
+    internal class number
+    {
+        public int[] array(int size)
+        {
+            int[] a = new int[size * 2];
+            for (int i = 0; i < a.Length; i+=2)
+            {
+                Console.Write("enter the value :");
+                int value = Convert.ToInt32(Console.ReadLine());
+                a[i] = value;
+                a[i + 1] = 0;
+            }
+            return a;
+        }
+    }
+}
