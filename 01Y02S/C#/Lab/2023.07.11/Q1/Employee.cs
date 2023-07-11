afc756762b966a2f6259c78669f465a8a346cf91
Dinuvi9170 <135813670+Dinuvi9170@users.noreply.github.com>
2023-07-11 10:32:54 +0530

    Add files via upload

---
+++ 01Y02S/C#/Lab/2023.07.11/Q1/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace my_program
+{
+    internal class Employee
+    {
+        private String empid;
+        private String empname;
+        private String empDesignation;
+         public void setid(String id) 
+        {
+            empid = id;
+        }
+        public String getid()
+        {
+            return empid;
+        }
+        public void setname(String name)
+        {
+           empname = name;
+        }
+        public String getname()
+        {
+            return empname;
+        }
+        public void setdesignation(String designation)
+        {
+            empDesignation = designation;
+        }
+        public String getdesignation()
+        {
+            return empDesignation;
+        }
+    }
+}
---
+++ 01Y02S/C#/Lab/2023.07.11/Q1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter your employee ID:");
+            String id = Console.ReadLine();
+            Console.WriteLine("Enter your employee designation:");
+            String designation = Console.ReadLine();
+
+
+            Employee obj = new Employee();
+            obj.setid(id );
+            obj.setname(name);
+            obj.setdesignation(designation);
+            Console.WriteLine("Your name is " + obj.getname());
+            Console.WriteLine("Your employee ID is " + obj.getid());
+            Console.WriteLine("Your designation is " + obj.getdesignation());
+
+            Console.ReadKey();
+        }
+    }
+}
