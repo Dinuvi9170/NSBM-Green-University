23db86ab3d4c70d960b9003f406b94b47770ac81
Dinuvi9170 <135813670+Dinuvi9170@users.noreply.github.com>
2023-07-11 11:00:13 +0530

    Add files via upload

---
+++ 01Y02S/C#/Lab/2023.07.11/Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter number 2:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Operation obj = new Operation();
+            obj.Setnum1(num1,num2);
+            Console.WriteLine("the summation:"+obj.getSum());
+            Console.WriteLine("the substraction:" + obj.getSub());
+            Console.WriteLine("the multipication:" + obj.getMul());
+            Console.WriteLine("the division:" + obj.getDiv());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01Y02S/C#/Lab/2023.07.11/Q2/operation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace my_program
+{
+    internal class Operation
+    {
+        private double number1;
+        private double number2;
+
+        public void Setnum1(double num1,double num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+        
+        public double getSum()
+        {
+            return number1+number2;
+        }
+        public double getSub()
+        {
+            return number1 - number2;
+        }
+        public double getMul()
+        {
+            return number1 * number2;
+        }
+        public double getDiv()
+        {
+            return number1 /number2;
+        }
+    }
+}  
